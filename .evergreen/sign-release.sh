43d172a69de9e8eddb522ebbf62240372713d8f5
Isabel Atkinson <isabel.atkinson@mongodb.com>
2025-11-19 16:49:00 -0700

    update release script

--- .evergreen/sign-release.sh
+++ .evergreen/sign-release.sh
@@ -7,12 +7,12 @@ if [[ -z "$CRATE" ]]; then
     echo >&2 "CRATE is required"
     exit 1
 fi
-if [[ -z "$ARTIFACTORY_PASSWORD" ]]; then
-    echo >&2 "ARTIFACTORY_PASSWORD is required"
+if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
+    echo >&2 "AWS_ACCESS_KEY_ID is required"
     exit 1
 fi
-if [[ -z "$ARTIFACTORY_USERNAME" ]]; then
-    echo >&2 "ARTIFACTORY_USERNAME is required"
+if [[ -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+    echo >&2 "AWS_SECRET_ACCESS_KEY is required"
     exit 1
 fi
 if [[ -z "$GARASIGN_USERNAME" ]]; then
@@ -26,7 +26,7 @@ fi
 
 CRATE_VERSION=$(cargo metadata --format-version=1 --no-deps | jq --raw-output '.packages[0].version')
 
-echo "${ARTIFACTORY_PASSWORD}" | docker login --password-stdin --username ${ARTIFACTORY_USERNAME} artifactory.corp.mongodb.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 901841024863.dkr.ecr.us-east-1.amazonaws.com
 
 echo "GRS_CONFIG_USER1_USERNAME=${GARASIGN_USERNAME}" >> "signing-envfile"
 echo "GRS_CONFIG_USER1_PASSWORD=${GARASIGN_PASSWORD}" >> "signing-envfile"
@@ -36,7 +36,7 @@ docker run \
   --rm \
   -v $(pwd):$(pwd) \
   -w $(pwd) \
-  artifactory.corp.mongodb.com/release-tools-container-registry-local/garasign-gpg \
+  901841024863.dkr.ecr.us-east-1.amazonaws.com/release-infrastructure/garasign-gpg \
   /bin/bash -c "gpgloader && gpg --yes -v --armor -o ${CRATE}-${CRATE_VERSION}.sig --detach-sign target/package/${CRATE}-${CRATE_VERSION}.crate"
 
 rm signing-envfile
